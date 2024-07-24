6fe9cafcb69429d7a8349d11fb5f4093ff16c960
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-24 02:38:36 -0300

    style(project): two spaces indent

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -5,63 +5,63 @@ import java.util.UUID;
 
 public class Category {
 
-    private String id;
-    private String name;
-    private String description;
-    private boolean active;
-    private Instant createdAt;
-    private Instant updatedAt;
-    private Instant deletedAt;
+  private String id;
+  private String name;
+  private String description;
+  private boolean active;
+  private Instant createdAt;
+  private Instant updatedAt;
+  private Instant deletedAt;
 
-    private Category(
-            final String id,
-            final String name,
-            final String description,
-            final boolean active,
-            final Instant createdAt,
-            final Instant updatedAt,
-            final Instant deletedAt
-    ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
-    }
+  private Category(
+          final String id,
+          final String name,
+          final String description,
+          final boolean active,
+          final Instant createdAt,
+          final Instant updatedAt,
+          final Instant deletedAt
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.active = active;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
 
-    public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
-        final var id = UUID.randomUUID().toString();
-        final var now = Instant.now();
-        return new Category(id, aName, aDescription, isActive, now, now, null);
-    }
+  public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
+    final var id = UUID.randomUUID().toString();
+    final var now = Instant.now();
+    return new Category(id, aName, aDescription, isActive, now, now, null);
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public boolean isActive() {
-        return active;
-    }
+  public boolean isActive() {
+    return active;
+  }
 
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
+  public Instant getCreatedAt() {
+    return createdAt;
+  }
 
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
+  public Instant getUpdatedAt() {
+    return updatedAt;
+  }
 
-    public Instant getDeletedAt() {
-        return deletedAt;
-    }
+  public Instant getDeletedAt() {
+    return deletedAt;
+  }
 }
