b17924518df79ac564b450af65328461876dc5ad
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-25 23:28:52 -0300

    fix(domain/tests): remove unused import

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
@@ -1,11 +1,10 @@
 package com.fullcycle.admin.catalogo.domain.category;
 
-import com.fullcycle.admin.catalogo.domain.exception.DomainException;
-import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
+import com.fullcycle.admin.catalogo.domain.exception.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 
 public class CategoryTest {
 
